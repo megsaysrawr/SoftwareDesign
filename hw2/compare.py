bbfc884c406a3b0a678311a2c873f33272f8562d
Meg McCauley <megan@mccauley.net>
2014-09-16 01:07:34 -0400

    Exercise 6.1, first pass

---
+++ hw2/compare.py
@@ -0,0 +1,15 @@
+"""
+compare.py
+Think Python, Exercise 6.1
+author = megsaysrawr
+"""
+
+def compare(x,y):
+    if x > y:
+        print 1
+    elif x == y:
+        print 0
+    else:
+        print -1
+        
+compare(1,2)
