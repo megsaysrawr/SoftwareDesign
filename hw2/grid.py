92e5c47d166d8014c47b474bdff008ff7dc775b4
Meg McCauley <megan@mccauley.net>
2014-09-16 14:16:49 -0400

    Exercise 3.5, second pass, added user input to make any size grid

--- hw2/grid.py
+++ hw2/grid.py
@@ -1,26 +1,26 @@
 """
 grid.py
 Think Python, Exercise 3.5
-author = megsaysrawr
+author = Meg McCauley
 """
 
-def topMiddleBottomRow(): #Creates the top, middle, and bottom rows
+def plusMinusRow(cols):
     segment = '+ ' + '- '*4
-    fullRow = segment * 2 + '+'
+    fullRow = segment * cols + '+'
     print fullRow
-
-def pipeRow(): #Creates the rows with pipes
-    segment2 = '|' + ' '*9
-    fullRow2 = segment2 * 2 + '|'
-    print fullRow2
     
-def rowMultipler(repeat): #Outputs a function a user defined number of times
-    for i in range(0,repeat):
-        pipeRow()
+def printPipeRows(cols):
+    for i in range(4):  
+        segment2 = '|' + ' '*9
+        fullRow2 = segment2 * cols + '|'
+        print fullRow2
     
-
-topMiddleBottomRow()
-rowMultipler(4)
-topMiddleBottomRow()
-rowMultipler(4)
-topMiddleBottomRow()
+def grid(rows,cols): #Creates a grid 
+    for i in range(rows):
+        plusMinusRow(cols)
+        printPipeRows(cols)
+    plusMinusRow(cols)
+     
+userRows = input("How many rows? ")
+userCols = input("How many columns? ")
+grid(userRows,userCols)
